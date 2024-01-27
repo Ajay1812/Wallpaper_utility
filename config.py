58d2d46d1d68a1ea810784e52d9c2ae435fade79
Ajay <81603467+Ajay1812@users.noreply.github.com>
2024-01-27 12:09:55 +0530

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# .gitignore should include reference to config.py
-application_id = "558528"
-api_access_key = "IPAD1Z6vXk-1H4jv0_tcDgltDq4ZXayZdT_UlpKMC6M"
-secret_key = "D2BtCoaHF_D-ehXB7Y8x1BK2gJAw4sJbIsIyaicujV0"
