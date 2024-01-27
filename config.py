ad41dcbed238c9a7516a1e57d360f97933ac0720
Ajay1812 <a.kumar01c@gmail.com>
2024-01-27 12:05:12 +0530

    Addess

---
+++ config.py
@@ -0,0 +1,4 @@
+# .gitignore should include reference to config.py
+application_id = "558528"
+api_access_key = "IPAD1Z6vXk-1H4jv0_tcDgltDq4ZXayZdT_UlpKMC6M"
+secret_key = "D2BtCoaHF_D-ehXB7Y8x1BK2gJAw4sJbIsIyaicujV0"
